750a50d439a7f9ec2a6b42eebf0af562c5c5c027
inwookie <31870931+inwookie@users.noreply.github.com>
2021-09-02 22:39:39 +0900

    Update server.js

--- src/server.js
+++ src/server.js
@@ -2,8 +2,22 @@ import express from "express";
 
 const PORT = 4000;
 
+// Creating an express app
 const app = express();
 
+// This creates requests function to each page
+const handleHome = (req, res) => {
+  return res.send("<h1>Maybe this work</h1>");
+};
+const handleLogin = (req, res) => {
+  return res.send("Login Page");
+};
+
+// This creates a page for my server
+app.get("/", handleHome);
+app.get("/login", handleLogin);
+
+// Asking localhost:4000 to listen to what my requests are
 const handleListening = () =>
   console.log(`Server Listening on port http://localhost:${PORT}`);
 
