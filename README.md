2d974f7b69e60eed58a522adb6ba9988f86bf3b1
inwookie <31870931+inwookie@users.noreply.github.com>
2021-09-05 23:20:04 +0900

    Added Routers

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # youtube-clone
+
 Youtube Clone built using NodeJS, Express, Mongo and ES6
+
+/ -> Home
+/join -> Join
+/login -> Login
+/search -> Searchm
+
+/users/edit -> Edit Profile
+/users/delete -> Delete Profile
+
+/videos/watch -> Watch Video
+/videos/edit -> Edit Video
+/videos/delete -> Delete Video
+/videos/comments -> Comment on a Video
+/videos/comments/delete -> Delete a Comment on a Video
--- src/server.js
+++ src/server.js
@@ -9,18 +9,25 @@ const app = express();
 // As it calls on next function, it is a middleware. It doesn't respond to the request. It just sends the job to the next function.
 const logger = morgan("dev");
 
-// This creates requests function to each page. This is also called controllers.
-const home = (req, res) => {
-  return res.send("hello world!");
-};
-const login = (req, res) => {
-  return res.send("login");
-};
-
 // This creates a page for my server.
 app.use(logger);
-app.get("/", home);
-app.get("/login", login);
+
+// This creates a router explained in README.md
+const globalRouter = express.Router();
+const handleHome = (req, res) => res.send("Home");
+globalRouter.get("/", handleHome);
+
+const userRouter = express.Router();
+const handleEditUser = (req, res) => res.send("Edit User");
+userRouter.get("/edit", handleEditUser);
+
+const videoRouter = express.Router();
+const handleWatchVideo = (req, res) => res.send("Watch Video");
+videoRouter.get("/watch", handleWatchVideo);
+
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
 
 // Asking localhost port of 4000 to listen to what my requests are.
 const handleListening = () =>
