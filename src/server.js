4221083cb1eab4099484dff643b5d37d6c4a8658
inwookie <31870931+inwookie@users.noreply.github.com>
2021-09-05 16:08:38 +0900

    Update server.js
    Added more middleware

--- src/server.js
+++ src/server.js
@@ -6,8 +6,17 @@ const PORT = 4000;
 const app = express();
 
 // As it calls on next function, it is a middleware. It doesn't respond to the request. It just sends the job to the next function.
-const gossipMiddleware = (req, res, next) => {
-  console.log("I'm in the middle");
+const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+};
+
+const privateMiddleware = (req, res, next) => {
+  const url = req.url;
+  if (url === "/protected") {
+    return res.send("<h1>Not Allowed</h1>");
+  }
+  console.log("Allowed");
   next();
 };
 
@@ -16,8 +25,15 @@ const handleHome = (req, res) => {
   return res.send("I love middlewares");
 };
 
+const handleProtected = (req, res) => {
+  return res.send("<Welcome to the private lounge.");
+};
+
 // This creates a page for my server.
-app.get("/", gossipMiddleware, handleHome);
+app.use(logger);
+app.use(privateMiddleware);
+app.get("/", handleHome);
+app.get("/protected", handleProtected);
 
 // Asking localhost:4000 to listen to what my requests are.
 const handleListening = () =>
