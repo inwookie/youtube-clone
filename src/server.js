9e673bf42fa5f250302eafe9aa7548e5aff7cafd
inwookie <31870931+inwookie@users.noreply.github.com>
2021-09-05 15:58:09 +0900

    Update server.js
    Added middleware

--- src/server.js
+++ src/server.js
@@ -2,22 +2,24 @@ import express from "express";
 
 const PORT = 4000;
 
-// Creating an express app
+// Creating an express app.
 const app = express();
 
-// This creates requests function to each page
+// As it calls on next function, it is a middleware. It doesn't respond to the request. It just sends the job to the next function.
+const gossipMiddleware = (req, res, next) => {
+  console.log("I'm in the middle");
+  next();
+};
+
+// This creates requests function to each page. This is also called controllers.
 const handleHome = (req, res) => {
-  return res.send("<h1>Maybe this work</h1>");
-};
-const handleLogin = (req, res) => {
-  return res.send("Login Page");
+  return res.send("I love middlewares");
 };
 
-// This creates a page for my server
-app.get("/", handleHome);
-app.get("/login", handleLogin);
+// This creates a page for my server.
+app.get("/", gossipMiddleware, handleHome);
 
-// Asking localhost:4000 to listen to what my requests are
+// Asking localhost:4000 to listen to what my requests are.
 const handleListening = () =>
   console.log(`Server Listening on port http://localhost:${PORT}`);
 
