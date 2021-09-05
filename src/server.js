36d7ed338e289225a04b4412b943f056f42ff246
inwookie <31870931+inwookie@users.noreply.github.com>
2021-09-05 16:15:48 +0900

    Update server.js

--- src/server.js
+++ src/server.js
@@ -11,29 +11,14 @@ const logger = (req, res, next) => {
   next();
 };
 
-const privateMiddleware = (req, res, next) => {
-  const url = req.url;
-  if (url === "/protected") {
-    return res.send("<h1>Not Allowed</h1>");
-  }
-  console.log("Allowed");
-  next();
-};
-
 // This creates requests function to each page. This is also called controllers.
 const handleHome = (req, res) => {
   return res.send("I love middlewares");
 };
 
-const handleProtected = (req, res) => {
-  return res.send("<Welcome to the private lounge.");
-};
-
 // This creates a page for my server.
 app.use(logger);
-app.use(privateMiddleware);
 app.get("/", handleHome);
-app.get("/protected", handleProtected);
 
 // Asking localhost:4000 to listen to what my requests are.
 const handleListening = () =>
