94a52355e68e0f6a68b801b897df9998a865d495
inwookie <31870931+inwookie@users.noreply.github.com>
2021-09-06 11:12:20 +0900

    Update routers

--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -1,11 +1,13 @@
 import express from "express";
-import { join } from "../controllers/userController";
-import { trending } from "../controllers/videoController";
+import { join, login } from "../controllers/userController";
+import { trending, search } from "../controllers/videoController";
 
 // This creates a router explained in README.md
 const globalRouter = express.Router();
 
 globalRouter.get("/", trending);
 globalRouter.get("/join", join);
+globalRouter.get("/login", login);
+globalRouter.get("/search", search);
 
 export default globalRouter;
